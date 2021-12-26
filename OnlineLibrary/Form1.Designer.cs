770349a4ccf5c381b0c16f4b866f549bb7c583b6
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2021-12-26 22:57:02 +0800

    Add project files.

---
+++ OnlineLibrary/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace OnlineLibrary
+{
+    partial class FormLibrarySystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelForm = new System.Windows.Forms.Panel();
+            this.picboxLogo = new System.Windows.Forms.PictureBox();
+            this.pictureBackground = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelLogo = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelForm.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBackground)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelForm
+            // 
+            this.panelForm.BackColor = System.Drawing.Color.White;
+            this.panelForm.Controls.Add(this.panel2);
+            this.panelForm.Controls.Add(this.panel1);
+            this.panelForm.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelForm.Location = new System.Drawing.Point(842, 0);
+            this.panelForm.Name = "panelForm";
+            this.panelForm.Size = new System.Drawing.Size(422, 681);
+            this.panelForm.TabIndex = 1;
+            // 
+            // picboxLogo
+            // 
+            this.picboxLogo.Location = new System.Drawing.Point(3, 3);
+            this.picboxLogo.Name = "picboxLogo";
+            this.picboxLogo.Size = new System.Drawing.Size(134, 109);
+            this.picboxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picboxLogo.TabIndex = 0;
+            this.picboxLogo.TabStop = false;
+            this.picboxLogo.Click += new System.EventHandler(this.picboxLogo_Click);
+            // 
+            // pictureBackground
+            // 
+            this.pictureBackground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBackground.Location = new System.Drawing.Point(0, 0);
+            this.pictureBackground.Name = "pictureBackground";
+            this.pictureBackground.Size = new System.Drawing.Size(1264, 681);
+            this.pictureBackground.TabIndex = 0;
+            this.pictureBackground.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.labelLogo);
+            this.panel1.Controls.Add(this.picboxLogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(422, 115);
+            this.panel1.TabIndex = 2;
+            // 
+            // labelLogo
+            // 
+            this.labelLogo.AutoSize = true;
+            this.labelLogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLogo.ForeColor = System.Drawing.Color.Navy;
+            this.labelLogo.Location = new System.Drawing.Point(143, 38);
+            this.labelLogo.Name = "labelLogo";
+            this.labelLogo.Size = new System.Drawing.Size(267, 46);
+            this.labelLogo.TabIndex = 1;
+            this.labelLogo.Text = "Online Library";
+            this.labelLogo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 115);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(422, 566);
+            this.panel2.TabIndex = 3;
+            // 
+            // FormLibrarySystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.panelForm);
+            this.Controls.Add(this.pictureBackground);
+            this.Name = "FormLibrarySystem";
+            this.Text = "Library System";
+            this.panelForm.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picboxLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBackground)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBackground;
+        private System.Windows.Forms.Panel panelForm;
+        private System.Windows.Forms.PictureBox picboxLogo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelLogo;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
